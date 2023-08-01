2112ad1057e7747d23492818afafcd08340cd4ad
Matt Wesley <meugenewesley@gmail.com>
2023-08-01 19:59:22 -0400

    added content to resume and about me.  added styling

--- src/components/About.js
+++ src/components/About.js
@@ -4,9 +4,9 @@ const About = () => {
   return (
     <div className="about-me">
         <section>
-          <h2>ABOUT ME</h2>
-          <p>Coding Enthusiast who needs a job!</p>
-
+          <h2>ABOUT ME:</h2>
+          <p>Coding Enthusiast who currently works in the finance world.  I enjoy fishing, playing golf, spending time with friends/family.</p>
+<img src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.sFfm-4yO9NcweYdDTLRIrQAAAA%26pid%3DApi&f=1&ipt=8e142b49674f05bdb34d8d392a45bd69d7c375ab45e5b618612293441a88f5c9&ipo=images'></img>
         </section>
     </div>
   );
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,12 +1,22 @@
-import './Resume.css';
+import "./Resume.css";
 
-import "./Resume.css"
+import "./Resume.css";
 const Resume = () => {
   return (
-    <div class="resume" >
-        <a href="/resume.pdf" download>DOWNLOAD MY RESUME</a>
+    <div className="resume">
+      <h2>Proficiencies</h2>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>React</li>
+        <li>MERN</li>
+      </ul>
+      <h2>Please click the link to view my resume below:</h2>
+      <a href="/resume.pdf" download>
+        DOWNLOAD MY RESUME
+      </a>
     </div>
   );
-}
+};
 
 export { Resume };
